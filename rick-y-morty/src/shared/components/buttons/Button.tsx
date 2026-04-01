935832301089119910458a90cc7deee579ca809c
Oscar Mata Alegria <oscar_1322134219@uptecamac.edu.mx>
2026-04-01 00:51:02 -0600

    feat: base button and their base styles

--- rick-y-morty/src/shared/components/buttons/Button.tsx
+++ rick-y-morty/src/shared/components/buttons/Button.tsx
@@ -1,14 +1,25 @@
 import { ReactNode } from "react";
+import styles from "@/src/shared/components/buttons/styles/Button.module.css"
+import { IconType } from "react-icons"
 
 interface ButtonContent {
     label: string;
     className?: string;
     onClick?: () => void;
-
+    type: "primary" | "secondary" | "cancel";
+    icon?: IconType;
+    iconSize?: number
 }
 
-export default function Button({ label, onClick, className }: ButtonContent) {
+export default function Button({ label, onClick, className = '', icon: Icon, type = "primary", iconSize = 20 }: ButtonContent) {
 
+    const buttonStyle = styles[type];
 
+    return (
+        <button className={`${className} ${styles.baseButton} ${buttonStyle}`} onClick={onClick} >
+            {Icon && <Icon size={iconSize} className={styles.icon} />}
+            <span>{label}</span>
+        </button>
+    )
 
 }
